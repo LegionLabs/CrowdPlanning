c54c857f290ce12a6d55bec7b718da894a04caf3
Walter S. Lasecki <wlasecki@roc.cs.rochester.edu>
2014-11-12 17:57:33 -0500

    updates to actions table and php to use more flexible sessions

--- ajax_requests.php
+++ ajax_requests.php
@@ -46,17 +46,22 @@ function json_response($error,$msg){
     die;
 }
 
+
 /*
  * function to save value inserted by the user for action
  */
-
 function user_action_save(){
 
     $length    = 50;
     $action    = $_REQUEST['value'];
     $session   = $_REQUEST['session'];
-    $user_hash = md5('somehash');
     $date      = strtotime(date('H:i:s')) ;
+    if( isset($_REQUEST['user']) ) {
+        $user_name = $_REQUEST['user'];
+    }
+    else {
+        $user_name = md5('somehash');
+    }
 
     if(!empty($action)){
 
@@ -68,22 +73,24 @@ function user_action_save(){
 
         }
 
+/*
         $user_id = getUserid($session);
 
         if($user_id != false){
 
-            $sth = $db->prepare("INSERT INTO actions (action_name, action_datetime, action_status, action_userid) VALUES(:action_name,:action_datetime,:action_status,:action_userid)");
-            $result = $sth->execute(array(':action_name' => $action, ':action_datetime' => $date, ':action_status' => 1, ':action_userid' => $user_id));
-
-            if($result){
-
-                $data = get_user_actions($user_id);
-                json_response(false,$data);
-
-            }
-
         }else{
             json_response(true,'user sessions doesnt exists!');
+        }
+*/
+
+        // WSL: For now, don't worry about the prior user sessions.
+
+        $sth = $db->prepare("INSERT INTO actions (action_name, action_datetime, action_status, action_session, action_userid) VALUES(:action_name,:action_datetime,:action_status,:action_session,:action_userid)");
+        $result = $sth->execute(array(':action_name' => $action, ':action_datetime' => $date, ':action_status' => 1, ':action_session'=>$session, ':action_userid' => $user_name));
+
+        if($result){
+            $data = get_session_actions($session);
+            json_response(false,$data);
         }
 
 
@@ -103,10 +110,11 @@ function connection(){
 
 }
 
+
+// WSL-TODO: Instead of getting the userID based on prior sessions (so that we can recover previously-added actions), let's try to get all prior sessions that were created for/by that user. Note that this might require us to use BOTH userID and sessionID as keys in the session table so that a user named "Tim" creating the session "demo" does not overlap with someone named "Raja" creating a different session, also called demo. THIS IS A FUTURE TASK, not something we need to deal with right now.
 /*
  * function to get id of the user
  */
-
 function getUserid($session){
 
     $db = connection();
@@ -132,16 +140,16 @@ function getUserid($session){
 
 }
 
+
 /*
  * Get user actions based on provided session hash
  */
-
-function get_user_actions($userid){
+function get_session_actions($session){
 
     $db = connection();
 
-    $sth = $db->prepare("SELECT * FROM actions WHERE action_userid=:sessionName LIMIT 30");
-    $sth->execute(array(':sessionName'=>$userid));
+    $sth = $db->prepare("SELECT * FROM actions WHERE action_session=:session LIMIT 30");
+    $sth->execute(array(':session'=>$session));
 
     $data =  array();
 
